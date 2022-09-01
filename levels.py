9e7fe27eec153b240689b1ad3457626c54293278
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2022-09-01 15:55:29 +0100

    Add daily level command and resolve errors thrown when entities were not found

--- levels.py
+++ levels.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.app_commands import command, describe
 
+from gd.errors import MissingAccess
+
 class Levels(commands.Cog):
 	def __init__(self, bot, gd):
 		self.bot = bot
@@ -9,23 +11,31 @@ class Levels(commands.Cog):
 
 	@command(description = "Retrieve information about a level")
 	async def levelinfo(self, ctx, id: int):
-		level = await self.gd.get_level(id)
-
-		if level is None:
-			embed = discord.Embed(color = discord.Colour.red())
+		try:
+			level = await self.gd.get_level(id)
+			embed = discord.Embed(colour = discord.Colour.green())
+			embed.add_field(name = "Name", value = level.name)
+			embed.add_field(name = "Downloads", value = level.downloads)
+			embed.add_field(name = "Rating", value = level.rating)
+			embed.add_field(
+				name = "Stars",
+				value = (f"{level.requested_stars} (requested)" if level.stars == 0 else level.stars)
+			)
+			embed.add_field(name = "Coins", value = level.coins)
+			embed.add_field(name = "Creator", value = level.creator.name)
+			embed.add_field(name = "Song ID", value = level.song.id)
+		except (MissingAccess, AssertionError):
+			embed = discord.Embed(colour = discord.Colour.red())
 			embed.add_field(name = "Error", value = "That level was not found.")
-			await ctx.response.send_message(embed = embed, ephemeral = True)
-			return
-		
-		embed = discord.Embed(color = discord.Colour.green())
-		embed.add_field(name = "Name", value = level.name)
-		embed.add_field(name = "Downloads", value = level.downloads)
-		embed.add_field(name = "Rating", value = level.rating)
-		embed.add_field(
-			name = "Stars",
-			value = (f"{level.requested_stars} (requested)" if level.stars == 0 else level.stars)
+		finally:
+			await ctx.response.send_message(embed = embed)
+	
+	@command(description = "Check the daily level")
+	async def dailylevel(self, ctx):
+		level = await self.gd.get_daily()
+		embed = discord.Embed(
+			colour = discord.Colour.green(),
+			title = f"{level.name}: {level.id}",
+			description = "Use </levelinfo:1014468288631279696> to view more information about the level"
 		)
-		embed.add_field(name = "Coins", value = level.coins)
-		embed.add_field(name = "Creator", value = level.creator.name)
-		embed.add_field(name = "Song ID", value = level.song.id)
 		await ctx.response.send_message(embed = embed)
--- songs.py
+++ songs.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.app_commands import command, describe
 
+from gd.errors import MissingAccess
+
 class Songs(commands.Cog):
 	def __init__(self, bot, gd):
 		self.bot = bot
@@ -9,16 +11,13 @@ class Songs(commands.Cog):
 	
 	@command(description = "Retrieve information about a song on Newgrounds")
 	async def songinfo(self, ctx, id: int):
-		song = await self.gd.get_song(id)
-
-		if song is None:
+		try:
+			song = await self.gd.get_song(id)
+			embed = discord.Embed(color = discord.Colour.green())
+			embed.title = f"{song.name} by {song.author}"
+			embed.description = song.link
+		except (MissingAccess, AssertionError):
 			embed = discord.Embed(color = discord.Colour.red())
 			embed.add_field(name = "Error", value = "That song was not found.")
-			await ctx.response.send_message(embed = embed, ephemeral = True)
-			return
-		
-		embed = discord.Embed(color = discord.Colour.green())
-		embed.title = f"{song.name} by {song.author}"
-		embed.description = song.link
-
-		await ctx.response.send_message(embed = embed)
+		finally:
+			await ctx.response.send_message(embed = embed)
